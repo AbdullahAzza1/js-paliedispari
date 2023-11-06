bfaa8a926f44379afed7164e436aebd914a442ca
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-06 15:42:19 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const parola = prompt("inserisci una parola")
+
+let parolaInvertita = invParola(parola)
+
+if (parola == parolaInvertita) {
+    console.log('la parola e Palindroma');
+} else {
+    console.log('la parola non e Palindroma');
+}
+function invParola(str) {
+    let strInversa = str.split('').reverse().join('');
+    return strInversa;
+}
+
+
+
+/*********pariOdispari**********/
